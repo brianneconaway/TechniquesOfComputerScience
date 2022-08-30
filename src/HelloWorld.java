f4c8e062cf24a3039dbfe396808cf37618864066
brian <brianneconaway@gmail.com>
2022-08-30 15:14:19 -0400

    Updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 // By convention, class names are capitalized
 public class HelloWorld {
-
+    /*
+    This is a multiline block comment
+    Another comment line.
+     */
     public static void main(String [] args) {
 
         System.out.println("Hello World!");
@@ -8,7 +11,12 @@ public class HelloWorld {
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022e23;
+        double electron_mass = 9.1e-31;
+
+
 
     }
-
 }
