cc0a720a81a724c6af96c2ef93c7b07919894178
brian <brianneconaway@gmail.com>
2022-09-07 17:42:58 -0400

    Homework 2

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,4 +1,34 @@
 package HW2;
 
+import java.util.Scanner;
+
 public class DayOfWeek {
-}
+    public static int week(int m,
+                              int d,
+                              int y) {
+
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        return (d + x + 31 * m0 / 12) % 7;
+
+    }
+
+        public static void main(String[] args){
+
+            Scanner kbd = new Scanner(System.in);
+
+            System.out.print("Enter a month: ");
+            int month = kbd.nextInt();
+
+            System.out.print("Enter a day: ");
+            int day = kbd.nextInt();
+
+            System.out.print("Enter a year: ");
+            int year = kbd.nextInt();
+
+
+            System.out.printf("The number of the week is: %d\n", (week(month, day, year)));
+
+        }
+    }
--- src/Utility/Weather.java
+++ src/Utility/Weather.java
@@ -3,8 +3,8 @@ package Utility;
 import java.util.Scanner;
 public class Weather {
 
-    public static double windchill(double v, //Temperature
-                                   double t) { //Velocity in MPH
+    public static double windchill(double t, //Temperature
+                                   double v) { //Velocity in MPH
 
 
         return 35.75 + 0.6215*t + (0.4275*t - 35.75)*Math.pow(v, 0.16);
